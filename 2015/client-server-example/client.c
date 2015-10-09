8899e7ab8f2ab0b7184ef33a001a0f3dc640936e
Goddesen <hugowallenburg@gmail.com>
2015-10-09 13:09:12 +0200

    Language changes, consistency++

--- 2015/client-server-example/client.c
+++ 2015/client-server-example/client.c
@@ -8,18 +8,18 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-int main(void) {
-    /* deklarasjon av litt datastruktur */
+int main(void)
+{
+    /* Deklarerer litt datastruktur */
     struct sockaddr_in serveraddr;
     int sock;
     char buf[12]; 
 
     /* Opprett socket */
-    sock = socket(AF_INET, SOCK_STREAM, 
-            IPPROTO_TCP);
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    /* Null ut serveradresse-struct'en */
-    memset((void *) &serveraddr, 0, sizeof(serveraddr));
+    /* Null ut serveradresse struct-en */
+    memset(&serveraddr, 0, sizeof(serveraddr));
 
     /* Sett domenet til Internett */
     serveraddr.sin_family = AF_INET;
@@ -31,21 +31,21 @@ int main(void) {
     serveraddr.sin_port = htons(2009);
 
     /* Koble opp */
-    connect(sock, (struct sockaddr *)&serveraddr, sizeof serveraddr); 
+    connect(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr)); 
 
     /* Send data */
     write(sock, "Hei verden!", 11);
 
-    /* les data fra forbindelsen */
+    /* Les data fra forbindelsen */
     read(sock, buf, 11);
 
     /* legg til et termineringstegn, og skriv ut til skjerm */
     buf[11] = '\0';
-    printf("%s \n",buf);
+    printf("%s\n",buf);
 
     /* Steng socketen */
     close(sock);
 
-    /* Program kjørte ordentlig - returner suksess-verdi */
+    /* Program kjørte ordentlig - returner suksessverdi */
     return EXIT_SUCCESS;
 }
