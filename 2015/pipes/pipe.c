f5937abea3553d6b27545278ef36e6a153449b6b
Goddesen <hugowallenburg@gmail.com>
2015-10-09 14:00:47 +0200

    pipe style fix & makefile

--- 2015/pipes/pipe.c
+++ 2015/pipes/pipe.c
@@ -8,38 +8,37 @@
 
 char *msg = "hello";
 
-int main()
+int main(void)
 {  
-    char inbuf[MSGSIZE];
-    int p[2];
-    pid_t pid;
+	char inbuf[MSGSIZE];
+	int p[2];
+	pid_t pid;
 
-    /* open pipe */ 	
-    if (pipe(p) == -1)
-    {
-        perror("pipe call error");
-        exit(1);
-    }
+	/* open pipe */ 	
+	if (pipe(p) < 0) {
+		perror("pipe call error");
+		exit(EXIT_FAILURE);
+	}
 
-    pid = fork();
-    switch(pid) {
-        case -1:
-            perror("error: fork call");
-            exit(2);
+	pid = fork();
+	switch(pid) {
+	case -1:
+		perror("error: fork call");
+		exit(2);
+	case 0: 
+		close(p[0]); /* Close the read end of the pipe */
 
-        case 0: 
-            close(p[0]); /* close the read end of the pipe */
+		write(p[1], msg, MSGSIZE);
+		printf("Child sent: %s\n", msg);
+		break;
+	default:
+		close(p[1]);  /* close the write end of the pipe */
 
-            write(p[1], msg, MSGSIZE);
-            printf("Child sent: %s\n", msg);
-            break;
+		read(p[0], inbuf, MSGSIZE);
+		printf("Parent read: %s\n", inbuf);
+		wait(0);
+		break;
+	}
 
-        default:
-            close(p[1]);  /* close the write end of the pipe */
-
-            read(p[0], inbuf, MSGSIZE);
-            printf("Parent read: %s\n", inbuf);
-            wait(0);
-    }
-    exit(0);
+	return EXIT_SUCCESS;
 }
